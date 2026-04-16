d82d0bd72b6a8de1acae0dafa27804c5ca354a50
ADMIN <ADMIN@PC333>
2026-04-16 15:43:27 +0700

    Add project files.

---
+++ Lab-01/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab_01.Models;
+
+namespace Lab_01.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<ProductModel> dsSanPham = new List<ProductModel> {
+           new ProductModel{ ID = 1, Name = "Nho", Price = 20000 },
+           new ProductModel{ ID = 2, Name = "Táo", Price = 50000 },
+           new ProductModel{ ID = 3, Name = "Chuối", Price = 90000 }
+        };
+        public IActionResult Index()
+        {
+            return View(dsSanPham);
+        }
+
+
+        //trả về danh sách sản phẩm
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //thêm mới sản phẩm vào danh sách
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                if(dsSanPham.Any(p => p.ID == product.ID))
+                {
+                    ModelState.AddModelError("Id", "Mã sản phẩm này đã tồn tại!");
+                    return View(product);
+                }
+                if (product.Name == null)
+                {
+                    ModelState.AddModelError("Name", "Tên sản phẩm không được để trống!");
+                    return View(product);
+                }
+
+                dsSanPham.Add(product);
+
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
---
+++ Lab-01/Models/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_01.Models
+{
+    public class ProductModel
+    {
+        [Display(Name = "Mã sản phẩm")]
+        [Required(ErrorMessage = "Vui lòng nhập mã sản phẩm.")]
+        public int ID { get; set; }
+
+        [Display(Name = "Tên sản phẩm")]
+        [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm.")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Giá sản phẩm")]
+        [Required(ErrorMessage = "Vui lòng nhập giá sản phẩm.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Giá sản phẩm không được là số âm.")]
+        public double Price { get; set; }
+    }
+}
